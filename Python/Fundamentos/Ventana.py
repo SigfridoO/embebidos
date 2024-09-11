3803378cfc014906918ecfdc2774326ebe481e2c
sigfrido <sigfrido_o@ardilluda.com.mx>
2024-09-11 10:44:37 -0600

    actualizando Boton

--- Python/Fundamentos/Ventana.py
+++ Python/Fundamentos/Ventana.py
@@ -1,10 +1,30 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, \
+    QPushButton
 import sys
 
+"""
+    Este es un
+    bloque de comentario
+"""
 class Ventana(QMainWindow):
     def __init__(self):
         super().__init__()
         print('Creando la ventana')
+        boton = QPushButton("Presioname")
+        boton.clicked.connect(self.clickear)
+        boton.released.connect(self.liberar)
+        boton.pressed.connect(self.presionar)
+
+        self.setCentralWidget(boton)
+
+    def clickear(self):
+        print ("Se ha clickeado el botón")
+
+    def presionar(self):
+        print ("Se ha presionado el botón")
+
+    def liberar(self):
+        print ("Se ha liberado el botón")
 
 def main():
     print('Dentro de main')

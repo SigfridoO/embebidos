122e75a17066c6657c7f93e1db6e85da65c4262b
sigfrido <sigfrido_o@ardilluda.com.mx>
2024-09-17 11:37:09 -0600

    actualizando camo de texto

--- Python/Fundamentos/Ventana.py
+++ Python/Fundamentos/Ventana.py
@@ -12,19 +12,27 @@ class Ventana(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        campo_texto = QLineEdit()
+        self.campo_texto = QLineEdit()
 
-
-        self.setCentralWidget(campo_texto)
+        self.setCentralWidget(self.campo_texto)
+        self.campo_texto.textChanged.connect(self.texto_cambiado)
+        self.campo_texto.returnPressed.connect(self.texto_presionado)
         self.setFixedSize(300, 150)
 
+    def texto_presionado(self):
+        texto = self.campo_texto.text()
+        self.setWindowTitle(texto)
+        print(texto)
+
+    def texto_cambiado(self, texto):
+        
+        print(texto)
 
 def main():
     app = QApplication(sys.argv)
     ventana = Ventana()
     ventana.show()
     sys.exit(app.exec())
-
 
 if __name__ == '__main__':
     main()

eb35829bee308e86c71453e267623d47799a753c
sigfrido <sigfrido_o@ardilluda.com.mx>
2024-09-17 15:15:53 -0600

    modificando configuración en arduiono

--- Python/Fundamentos/Ventana.py
+++ Python/Fundamentos/Ventana.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, \
-    QPushButton, QLabel, QLineEdit, QSpinBox, QDoubleSpinBox
+    QPushButton, QLabel, QLineEdit, QSpinBox, QDoubleSpinBox, \
+    QComboBox
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFont, QPixmap
 import sys
@@ -12,21 +13,23 @@ class Ventana(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.campo_numerico = QDoubleSpinBox()
-        self.campo_numerico.valueChanged.connect(self.valor_cambiado)
-        self.campo_numerico.setRange(10, 40)
-        self.campo_numerico.setSuffix("º")
-        self.campo_numerico.setPrefix("Temp: ")
-        self.campo_numerico.setSingleStep(0.5)
+        self.combo = QComboBox()
+        self.combo.addItems( ["", "Opción 1", "Opción 2", "Opcion 3"])
+        self.combo.currentTextChanged.connect(self.cambiar_texto)
+        self.combo.currentIndexChanged.connect(self.cambiar_indice)
 
-        fuente = QFont('Manjari', 36)
-        self.campo_numerico.setFont(fuente)
-        
-        self.setCentralWidget(self.campo_numerico)
+        self.setCentralWidget(self.combo)
         self.setFixedSize(300, 150)
 
-    def valor_cambiado(self, valor):
-        print(valor)
+    def cambiar_texto(self, texto):
+        print(texto)
+
+    def cambiar_indice(self, indice):
+        print(
+            #type(indice), 
+              indice)
+
+
 
 def main():
     app = QApplication(sys.argv)

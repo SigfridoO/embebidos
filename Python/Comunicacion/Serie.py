0aa609964ebd45e9ae8f58803ae510278d4ff259
sigfrido <sigfrido_o@ardilluda.com.mx>
2024-10-03 11:54:00 -0600

    actualizando comunicación

--- Python/Comunicacion/Serie.py
+++ Python/Comunicacion/Serie.py
@@ -1,6 +1,8 @@
 import serial
 import time
 
+from Convertidor import Convertidor
+
 def main():
     puerto_serie = serial.Serial()
     puerto_serie.port = "/dev/ttyUSB0"
@@ -10,26 +12,25 @@ def main():
     puerto_serie.stopbits = serial.STOPBITS_ONE
     puerto_serie.bytesize = serial.EIGHTBITS
     
-    cadena = bytearray(1)
+    #cadena = bytearray(1)
     # Aqui se abre el puerto serie
     puerto_serie.open()
 
-    cadena[0]= 62
+    #cadena[0]= 62
+    convertidor = Convertidor()
+    mensaje = convertidor.generar_mensaje(0, 12)
+    print("El mensaje es: ", mensaje)
     time.sleep(1)
     if puerto_serie.is_open:
-        puerto_serie.write(cadena)
+        puerto_serie.write(mensaje)
         time.sleep(0.01)
-        res = puerto_serie.read(1)
-        print(res, 
-              int.from_bytes(res), 
-              res.decode(), 
+        res = puerto_serie.read(20)
+        print("Respuesta >>: ", res, 
+              #int.from_bytes(res), 
+              #res.decode('utf-8'), 
+             " >> "
               ' '.join('{0:02X}'.format(e) for e in res))
     puerto_serie.close()
-
-
-
-
-
 
 if __name__=="__main__":
     main()
